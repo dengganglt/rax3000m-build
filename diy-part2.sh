dc30b84b78f5d79d4d2317ff33f93881d43b45ec
dengganglt <dengganglt@gmail.com>
2026-03-21 06:58:48 -0700

    Update diy-part2.sh for RAX3000M-1GB configuration
    Updated the script to modify the hostname and terminal banner for the RAX3000M-1GB. Applied a memory addressing patch for 1GB.

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,18 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP (kept as 192.168.1.1)
+# sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify hostname to RAX3000M-1GB
+sed -i 's/ImmortalWrt/RAX3000M-1GB/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Core: 1GB Memory Addressing Patch (精准匹配 memory 节点的 reg 参数)
+# Official DTS default: <0x0 0x40000000 0x0 0x20000000> (512MB)
+# Modified to: <0x0 0x40000000 0x0 0x40000000> (1GB)
+find target/linux/mediatek/dts/ -name "*rax3000m*.dts" | xargs sed -i 's/<0x0 0x40000000 0x0 0x20000000>/<0x0 0x40000000 0x0 0x40000000>/g'
+
+# Modify terminal banner
+sed -i "s/OpenWrt /RAX3000M-1GB-$(date +%Y%m%d) /g" package/base-files/files/etc/banner
+
+echo "Memory Patch Applied Success."
