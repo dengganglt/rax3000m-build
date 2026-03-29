0803e242607c981c4f1eba8a4423974409ff0fd4
dengganglt <dengganglt@gmail.com>
2026-03-29 05:27:33 -0700

    Refactor diy-part1.sh to clean up plugin sources
    Removed commented plugin sources and added cleanup logic to prevent conflicts.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# 1. 清理可能存在的重复项（防止多次执行产生冲突）
+# 即使精简，也建议保留清理逻辑，防止云端缓存干扰
 sed -i '/helloworld/d' feeds.conf.default
 sed -i '/passwall/d' feeds.conf.default
 
-# 2. 添加插件源（使用带 .git 后缀的完整 HTTPS 地址，提高兼容性）
-# Helloworld 插件源
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-
-# Passwall 插件源（主程序）
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-
-# Passwall 依赖包源（必须添加，否则会缺少依赖导致编译失败）
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+# 如果你想加点别的（比如非常规的主题或特殊插件），可以在这里添加，否则，留空即可
